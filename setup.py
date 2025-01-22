b64f6d2cf9a4783785102a67e9ac7fda74aecd66
KSKOP69 <123794242+KSKOP69@users.noreply.github.com>
2025-01-23 00:58:49 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="youtube-search-python",
+    version="1.6.9",
+    author="Hitesh Kumar Saini",
+    license='MIT',
+    author_email="saini123hitesh@gmail.com",
+    description="Search for YouTube videos, channels & playlists & get video information using link WITHOUT YouTube Data API v3",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/alexmercerind/youtube-search-python",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'httpx>=0.28.1'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.10',
+)
